5e6c854a32e8511dbf58145b641583417fcc7ef9
Mohammad Athar <atharshaikh283@gmail.com>
2025-05-25 22:46:04 +0530

    Initial Commit

---
+++ src/main/java/com/DemoApplication.java
@@ -0,0 +1,12 @@
+package com;// Import required classes and packages
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
